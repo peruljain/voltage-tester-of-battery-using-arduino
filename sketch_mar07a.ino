74c849b79a03d0369024fec1d922b6a4579357fb
nikarora1111 <nikarora1111@gmail.com>
2018-06-20 09:33:11 +0530

    Update sketch_mar07a.ino

--- sketch_mar07a.ino
+++ sketch_mar07a.ino
@@ -8,11 +8,12 @@
 void setup()
 {
   pinMode(ana,INPUT);
+  Serial.begin(9600);
   lcd.begin(16,2);
   lcd.setCursor(0,0);
   lcd.print("battery voltage");
   delay(1000);
-  value=analogRead(ana)*5*4;
+  value=analogRead(ana)*5*4; //multiplied by 4 because i am using 4 427-428 K OHM resistance to increase range upto 20 v. if i will not use this will have range of 0-5 V only
   value=value/1024;
   
 
